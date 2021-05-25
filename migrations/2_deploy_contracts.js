97305a82c13ff05152f3e3d54de3d140e9f8c355
tunde <icepaigez@gmail.com>
2021-05-25 21:20:27 +0100

    deployed token contract to the blockchain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,15 @@
 const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-module.exports = function(deployer) {
-  deployer.deploy(Token);
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  //on deployment of the token contract, all the tokens are saved in the account 
+  //of the deployer. but the tokens are needed on the cryptoexchange, so we move
+  //the tokens to the account of the exchange
+  await token.transfer(ethSwap.address, '1000000000000000000000000'); 
 };
