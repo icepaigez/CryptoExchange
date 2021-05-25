5c9b34ff0bad657de3df9ed1fa84fc1325c1d5f8
tunde <icepaigez@gmail.com>
2021-05-25 18:21:09 +0100

    setting up the token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 contract Token {
   string  public name = "DApp Token";
   string  public symbol = "DAPP";
-  uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+  uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens denominated in wei (the smallest unit of ether)
   uint8   public decimals = 18;
 
   event Transfer(
