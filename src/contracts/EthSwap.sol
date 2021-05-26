b09ab16646b41b57df1fe152dbec94cbbd836cbe
tunde <icepaigez@gmail.com>
2021-05-26 13:35:08 +0100

    updated the visibility of the exchange constructor

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -6,7 +6,7 @@ contract EthSwap {
 	string public name = "EthSwap Cryptocurrency Exchange";
 	Token public token;
 
-	constructor(Token _token) { //this is another form of inheriting from the Token contract, and we have to link the Token contract to this contract by passing in the address in the deployment file i.e set arg here and pass in the parameter before deploying to the blockchain
+	constructor(Token _token) public { //this is another form of inheriting from the Token contract, and we have to link the Token contract to this contract by passing in the address in the deployment file i.e set arg here and pass in the parameter before deploying to the blockchain
 		token = _token;
 	}
 }
