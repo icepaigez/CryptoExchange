6e3895abdddba301f4a9f2f8be9a503690a19ada
tunde <icepaigez@gmail.com>
2021-05-26 03:21:11 +0100

    added a helper function

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -5,6 +5,10 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
+function tokens(qty) {
+	return web3.utils.toWei(qty, "ether");
+}
+
 contract('EthSwap', accounts => {
 	let token, ethSwap
 	before(async() => {
@@ -27,9 +31,9 @@ contract('EthSwap', accounts => {
 
 	describe('Token Transfer to EthSwap', async () => {
 		it('EthSwap should hold all the tokens', async () => {
-			await token.transfer(ethSwap.address, "1000000000000000000000000")
+			await token.transfer(ethSwap.address, tokens("1000000"))
 			let balance = await token.balanceOf(ethSwap.address)
-			assert.equal(balance.toString(), "1000000000000000000000000")
+			assert.equal(balance.toString(), tokens("1000000"))
 		})
 	})
 })
