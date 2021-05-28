24eacdff186ce9457d567bec63e1ac2915c26c97
tunde <icepaigez@gmail.com>
2021-05-28 07:36:25 +0100

    set up function to sell tokens back to the exchange

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -13,12 +13,22 @@ contract EthSwap {
 		token = _token;
 	}
 
+	//buy tokens from the exchange i.e. pay ether to get tokens
 	function buyToken() public payable {
-		uint tokenAmount = msg.value * rate;
+		uint tokenAmount = msg.value * rate; //msg.value refers to the amount of ether sent to purchase tokens
 		//check that the exchange has at least this amount of tokens
-		require(token.balanceOf(address(this)) >= tokenAmount);
+		require(token.balanceOf(address(this)) >= tokenAmount); //address(this) is the exchange's token wallet
 		token.transfer(msg.sender, tokenAmount);
 		//emit an event after a transfer is done
 		emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+	}
+
+	//sell tokens back to the exchange i.e. give tokens and get back ether 
+	function sellToken(uint _amount) public {
+		uint etherAmount = _amount / rate;
+		//send ether to the seller
+		msg.sender.transfer(etherAmount); //this transfer is the function on the msg.sender global variable and NOT the transfer function from the ERC-20 token standard
+		//transfer tokens from seller to exchange
+		token.transferFrom(msg.sender, address(this), _amount);
 	}
 }
