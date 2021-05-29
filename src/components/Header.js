25e5dd0f3d133b414dcb8acca1e2e9a62db8e666
tunde <icepaigez@gmail.com>
2021-05-29 07:52:39 +0100

    created a style sheet for the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./header.css";
 
 const Header = () => {
 	return(
-		 <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+		 <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow header">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
             href="http://www.dappuniversity.com/bootcamp"
