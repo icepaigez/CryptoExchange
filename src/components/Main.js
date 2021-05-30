4999d5891fb6d414d30afccb590e8682f0bd2bc1
tunde <icepaigez@gmail.com>
2021-05-30 15:45:53 +0100

    added a conditional to switch between buy and sell forms

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import BuyForm from "./BuyForm";
+import SellForm from "./SellForm";
 import "./main.css";
 
 
 class Main extends Component {
+	constructor() {
+		super()
+		this.state = {
+			formState: "buy"
+		}
+	}
 	
 	render() {
 		const { ethBalance, tokenBalance, buytoken } = this.props;
+		const { formState } = this.state;
 		return(
 			<div className="main">
-		      <BuyForm ethBalance={ethBalance} tokenBalance={tokenBalance} buytoken={buytoken}/>
-	      </div>
+		      { formState === "buy" ? <BuyForm ethBalance={ethBalance} tokenBalance={tokenBalance} buytoken={buytoken}/> : <SellForm /> }
+	      	</div>
 		)
 	}
 }
